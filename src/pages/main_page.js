55145b4477808074ddf7ffbb1c357e0f0197d4cf
Alexey Ukrainets <alekseyukrainets@MacBook-Pro-Alexey.local>
2022-10-31 20:41:24 +0300

    test

--- src/pages/main_page.js
+++ src/pages/main_page.js
@@ -12,11 +12,11 @@ export default function Main() {
   return (
     <div className='mainPage'>
       <Header /> 
-      
       <div className='page'>
         <SideMenu/>
         <Outlet />
       </div>
+      
       <Footer />
     </div>
   )
